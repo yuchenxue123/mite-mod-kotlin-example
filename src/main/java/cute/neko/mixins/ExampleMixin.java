45b077f515a82b2d765c521a9aafc79f208777f8
yuchenxue <1816271774@qq.com>
2025-07-15 21:15:59 +0800

    create

---
+++ src/main/java/cute/neko/mixins/ExampleMixin.java
@@ -0,0 +1,21 @@
+package cute.neko.mixins;
+
+import net.minecraft.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * @author yuchenxue
+ * @date 2025/07/15
+ */
+
+@Mixin(Minecraft.class)
+public class ExampleMixin {
+
+    @Inject(method = "startGame", at = @At(value = "HEAD"))
+    public void startGame(CallbackInfo ci) {
+        // pppppppppppppppppppppppppppppppp
+    }
+}
---
+++ src/main/kotlin/cute/neko/example/ExampleMod.kt
@@ -0,0 +1,14 @@
+package cute.neko.example
+
+import net.fabricmc.api.ModInitializer
+
+/**
+ * @author yuchenxue
+ * @date 2025/07/15
+ */
+
+class ExampleMod : ModInitializer {
+    override fun onInitialize() {
+        println("This is a toooooooooooooooooooooooooo looooooooooooooooooooooooooooooooooong print")
+    }
+}
