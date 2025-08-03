6d083b74c94631778c4249b735f21a76326df00a
yuchenxue <1816271774@qq.com>
2025-08-04 00:27:42 +0800

    Kotlinize gradle

---
+++ build.gradle.kts
@@ -0,0 +1,100 @@
+plugins {
+    kotlin("jvm")
+    id("fml-loom")
+}
+
+val mod_name: String by project
+val mod_version: String by project
+
+version = mod_version
+
+base {
+    archivesName = mod_name
+}
+
+repositories {
+    maven {
+        name = "Jitpack"
+        url = uri("https://jitpack.io")
+    }
+}
+
+val access_widener: String by project
+
+loom {
+    accessWidenerPath = file("src/main/resources/$access_widener")
+    mergedMinecraftJar()
+    fml = File("libs/loader-v3.4.1.jar")
+
+    mods {
+        create(mod_name) {
+            sourceSet(sourceSets.main.get())
+        }
+    }
+}
+
+val packageImplementation = configurations.register("packageImplementation") {
+    configurations.implementation.get().extendsFrom(this)
+}
+
+fun DependencyHandler.packageImplementation(dependencyNotation: Any) {
+    add("packageImplementation", dependencyNotation)
+}
+
+val minecraft_version: String by project
+val kotlin_version: String by project
+
+dependencies {
+    minecraft("com.mojang:minecraft:$minecraft_version")
+    mappings(loom.fmlMCPMappings())
+    implementation(files(loom.fml.toPath()))
+
+    packageImplementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+}
+
+val mod_id: String by project
+val mod_description: String by project
+
+val properties = mapOf(
+    "id" to mod_id,
+    "name" to mod_name,
+    "version" to mod_version,
+    "description" to mod_description,
+    "widener" to access_widener
+)
+
+tasks.processResources {
+    inputs.property("version", mod_version)
+
+    filesMatching("fml.mod.json") {
+        expand(properties)
+    }
+}
+
+tasks.jar {
+
+    from({
+        packageImplementation.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    }) {
+        exclude("META-INF/")
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(17)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmToolchain(17)
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+    }
+
+    plugins {
+        val kotlin_version: String by settings
+        val loom_version: String by settings
+
+        kotlin("jvm") version kotlin_version
+        id("fml-loom") version loom_version
+    }
+}
