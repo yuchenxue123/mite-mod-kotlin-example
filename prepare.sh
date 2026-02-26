516f070f94d8f81411da51110fce1132e83919ff
yuchenxue <1816271774@qq.com>
2026-02-26 16:28:10 +0800

    test

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,12 @@
 
 GRADLE_HOME="${GRADLE_USER_HOME:-$HOME/.gradle}"
 
+echo "好1：$GRADLE_HOME"
+
 SOURCE_FILE="mite/mite.jar"
+
+echo "好2：SOURCE_FILE"
+
 TARGET_DIR="$GRADLE_HOME/caches/fml-loom/1.6.4-MITE/"
 TARGET_FILENAME="1.6.4-MITE+R196.jar"
 
@@ -10,6 +15,7 @@ mkdir -p "$TARGET_DIR"
 
 if [ ! -f "$SOURCE_FILE" ]; then
   echo "Cannot find source file: $SOURCE_PATH" >&2
+  echo "早不到"
   exit 1
 fi
-cp "$SOURCE_PATH" "$TARGET_DIR/$TARGET_FILENAME"
+  cp "$SOURCE_PATH" "$TARGET_DIR/$TARGET_FILENAME"
